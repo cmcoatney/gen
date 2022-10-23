bcdf40f9c7ea74c4638e021bb556b0c256af794a
Matthew Coatney <mcoatney@bitwiseindustries.com>
2022-10-22 20:52:03 -0700

    Initial commit

---
+++ apps/gen/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gen-x-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'gen';
+}
